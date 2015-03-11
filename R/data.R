829c91ad38473cffdc12039449c0a200b638a8fb
colinpmillar <millarc@marlab.ac.uk>
2015-03-11 12:55:20 +0000

    added file filters as data

---
+++ R/data.R
@@ -0,0 +1,11 @@
+
+#' FILTERS FOR CHOOSEING CALIBRATION FILES.
+#'
+#' note only available on windows.
+"myFilters"
+
+#myFilters <- matrix(c("UKAS calibration", "UKAS*.csv",
+#                      "Internal calibration", "full*.csv",
+#                      "all calibration", "*.csv",
+#                      "all files", "*.*"), ncol = 2, byrow = TRUE)
+#devtools::use_data(myFilters)
